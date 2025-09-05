7d24ed03c0124efaabc3fbf6409ffc338b120fca
indira-faizullina <indiramus@list.ru>
2025-09-05 09:20:35 +0300

    added useState for App component content

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,49 @@
 import Header from "./components/Header"
 import Direction from "./components/Direction"
+import Button from "./components/Button/Button.jsx"
 import { directions } from './data.js'
+import { useState } from "react"
 
 export default function App() {
 
-  return (
-    <>
-    <Header/>
-    <main>
-      <h1>Школа программирования для детей IT Genius.</h1>
-      <section>
-      <h3>Направления</h3>
-      {directions.map((direction, index) =>
-        <Direction 
-        key={index}
-        title={direction.title} 
-        age={direction.age} 
-        description = {direction.description}
-        img = {direction.img}
-        />
-      )}
-      </section>
-    </main>
-    </>
-  )
+  const [content, useContent] = useState('main')
+
+  const handleClick = function(title) {
+    useContent(title)
+  }
+
+  if(content === 'main') {
+    return (
+        <>
+        <Header/>
+        <main>
+          <h1>Школа программирования для детей IT Genius.</h1>
+          <section>
+          <h3>Направления</h3>
+          {directions.map((direction, index) =>
+            <div key={index}>
+              <Direction 
+              title={direction.title} 
+              age={direction.age} 
+              description = {direction.description}
+              img = {direction.img}
+              />
+              <Button onClick={() => handleClick(direction.title)}>Записаться на пробный урок</Button>
+            </div>
+          )}
+          </section>
+        </main>
+        </>
+      )
+      } else {
+        return(
+          <>
+          <Header/>
+          <div>
+            <h1>{content}</h1>
+          </div>
+          </>
+        )
+      }
+  
 }
--- src/components/Direction.jsx
+++ src/components/Direction.jsx
@@ -1,15 +1,11 @@
-import Button from "./Button/Button"
-
 export default function Direction({ title, age, description, img }) {
-    const handleClick = (title) => console.log(title)
     
     return(
-        <div>
+        <div id="directions">
             <h4>{title}</h4>
             <span>{age}</span>
             <p>{description}</p>
             <img src={`/${img}.jpg`} alt={`изображение направления ${title}`} />
-            <Button onClick={()=> handleClick({title})}>Записаться на пробный урок</Button>
         </div>
     )
 }
