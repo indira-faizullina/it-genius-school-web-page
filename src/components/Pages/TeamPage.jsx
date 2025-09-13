4eb910d42626c25458bea779954819f4c448e94b
indira-faizullina <indiramus@list.ru>
2025-09-13 14:45:48 +0300

    finished TeamPage component

--- src/components/Pages/TeamPage.jsx
+++ src/components/Pages/TeamPage.jsx
@@ -1,5 +1,33 @@
+import { useEffect, useState } from "react"
+
 export default function TeamPage() {
+
+const [persons, setPersons] = useState([])  
+const [loading, setLoading] = useState(false)
+
+useEffect(() => {
+    async function fetchPersons() {
+    setLoading(true)
+    const response = await fetch('https://my-json-server.typicode.com/indira-faizullina/fake-api-for-IT-genius-school/persons/')
+    const data = await response.json()
+    setPersons(data)
+    setLoading(false)
+}
+
+    fetchPersons()
+}, [])
+
     return(
+        <>
         <h1>Наша команда</h1>
+        {loading && <p>Loading...</p>}
+        {!loading && persons.map((person, i) => (
+            <div key={i}>
+                <img src={person.photo} alt="" />
+                <h3>{person.name}</h3>
+                <p>{person.description}</p>
+            </div>
+        ))}
+        </>
     )
 }
